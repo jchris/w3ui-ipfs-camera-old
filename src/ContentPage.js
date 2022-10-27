fb7e4025f620e7976f6b1097f5615206afe8eddb
Chris Anderson <jchris@gmail.com>
2022-10-27 12:02:58 +0100

    add camera component

--- src/ContentPage.js
+++ src/ContentPage.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useUploader } from '@w3ui/react-uploader'
 import { withIdentity } from './components/Authenticator'
+import { Camera } from 'react-camera-pro'
 import './spinner.css'
 
 export function ContentPage () {
@@ -9,6 +10,16 @@ export function ContentPage () {
   const [dataCid, setDataCid] = useState('')
   const [status, setStatus] = useState('')
   const [error, setError] = useState(null)
+  const [images, setImages] = useState([])
+
+  const camera = useRef(null)
+
+  const takePhoto = async (e) => {
+    e.preventDefault()
+    const imgdata = camera.current.takePhoto()
+    const cid = null
+    setImages([{ cid: cid, data: imgdata }, ...images])
+  }
 
   if (!uploader) return null
 
@@ -36,6 +47,8 @@ export function ContentPage () {
 
   return (
     <form onSubmit={handleUploadSubmit}>
+      <button onClick={takePhoto}>Take photo</button> 
+      <Camera ref={camera} />
       <div className='db mb3'>
         <label htmlFor='file' className='db mb2'>File:</label>
         <input id='file' className='db pa2 w-100 ba br2' type='file' onChange={e => setFile(e.target.files[0])} required />
